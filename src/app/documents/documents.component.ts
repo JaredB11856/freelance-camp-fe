43ea782f176037688557c3508668a7fc0e302fff
Jared Brown <Jb_90210@hotmail.com>
2018-04-25 20:57:11 -0600

    Using Bootstrap Cards in an Angular Application

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent {
       description: 'qwerty',
       file_url: 'http://google.com',
       updated_at: '11/11/18',
-      image_url: 'http:google.com'
+      image_url: 'https://c1.staticflickr.com/3/2586/4233673443_35845c6074_b.jpg'
     },
     {
       title: "My Second Doc",
       description: 'qwerty',
       file_url: 'http://google.com',
       updated_at: '11/11/18',
-      image_url: 'http:google.com'
+      image_url: 'https://www.maxpixel.net/static/photo/1x/Work-Freelancer-Businessman-Productivity-1995786.jpg'
     },
     {
       title: "My Last Doc",
       description: 'qwerty',
       file_url: 'http://google.com',
       updated_at: '11/11/18',
-      image_url: 'http:google.com'
+      image_url: 'https://c1.staticflickr.com/1/608/21167500651_ba42a5c4a0_b.jpg'
     },
   ]
 }
