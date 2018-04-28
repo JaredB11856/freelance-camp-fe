b89d4bfa0f647f8a2b7425aa3f93d80f145df713
Jared Brown <Jb_90210@hotmail.com>
2018-04-27 21:35:22 -0600

    How to Render API Data in an Angular 2 Application

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,36 +1,35 @@
-import { Component } from '@angular/core';
-import { Document } from './document'
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Document } from './document';
+import { DocumentService } from './document.service';
 
 @Component({
   moduleId: module.id,
   selector: 'documents',
   templateUrl: 'documents.component.html',
-  styleUrls: ['documents.component.css']
+  styleUrls: ['documents.component.css'],
+  providers: [ DocumentService ]
 })
-export class DocumentsComponent {
+export class DocumentsComponent implements OnInit {
   pageTitle: string = "Document Dashboard"
-  
-  documents: Document[] = [
-    {
-      title: "My First Doc",
-      description: 'qwerty',
-      file_url: 'http://google.com',
-      updated_at: '11/11/18',
-      image_url: 'https://c1.staticflickr.com/3/2586/4233673443_35845c6074_b.jpg'
-    },
-    {
-      title: "My Second Doc",
-      description: 'qwerty',
-      file_url: 'http://google.com',
-      updated_at: '11/11/18',
-      image_url: 'https://www.maxpixel.net/static/photo/1x/Work-Freelancer-Businessman-Productivity-1995786.jpg'
-    },
-    {
-      title: "My Last Doc",
-      description: 'qwerty',
-      file_url: 'http://google.com',
-      updated_at: '11/11/18',
-      image_url: 'https://c1.staticflickr.com/1/608/21167500651_ba42a5c4a0_b.jpg'
-    },
-  ]
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";  
+
+  constructor(
+  private documentService: DocumentService;
+  ) {}
+
+  ngOnInit () {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
+  }
+
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
